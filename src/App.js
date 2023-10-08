6f7d2e08118b7be4bf3090cb3a2eeda20a3e59f6
Deepak1095 <ddhankhar999@gmail.com>
2023-10-08 13:09:40 +0530

    AboutSession component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import AboutSession from './components/aboutSession';
 import Footer from './components/footer';
 import TrendingSpeaker from './components/trendingSpeaker';
 function App() {
   return (
 <div >
-
+<AboutSession/>
  <TrendingSpeaker/>
  <Footer />
- 
+
 </div>
 
 
