8e09d65d01eb83800f3f2595b9a3b0fedf40df43
Deepak1095 <ddhankhar999@gmail.com>
2023-10-08 14:43:26 +0530

    Clients component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/about';
 import AboutSession from './components/aboutSession';
+import Clients from './components/clients';
 import Footer from './components/footer';
 import RegistrationForm from './components/registrationForm';
 import TrendingSpeaker from './components/trendingSpeaker';
@@ -9,6 +10,7 @@ function App() {
 <div >
   <RegistrationForm/>
 <AboutSession/>
+<Clients/>
 <About/>
  <TrendingSpeaker/>
  <Footer />
---
+++ src/components/clients.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Clients = () => {
+  return (
+    <div class="flex flex-col items-center justify-center text-center md:mb-44">
+<p class="text-base sm:text-lg md:text-xl lg:text-4xl w-4/5 sm:w-3/4 md:w-1/2 lg:w-5/12 mb-6 mt-4 font-bold">Your SpeakIn is Trusted by the Best of Clients</p>
+<p class="text-sm sm:text-base md:text-lg lg:text-lg w-4/5 sm:w-3/4 md:w-1/2 lg:w-1/3 mb-4 font-medium">SpeakIn speakers have enthralled audiences across top-of-the-line businesses in varied sectors globally</p>
+    <img src="https://github.com/Deepak1095/esmagico/assets/111503473/868b02db-1eb5-4c05-9e16-aa9748f361e7" alt="" />
+</div>
+
+
+  )
+}
+
+export default Clients
