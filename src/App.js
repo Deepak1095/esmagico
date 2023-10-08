cb4fe3ab29b94e186ab658ad4d226f3c4a457ec4
Deepak1095 <ddhankhar999@gmail.com>
2023-10-08 14:56:09 +0530

    aboutLiveSession component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import About from './components/about';
+import AboutLiveSession from './components/aboutLiveSession';
 import AboutSession from './components/aboutSession';
 import Clients from './components/clients';
 import Footer from './components/footer';
 import Learning from './components/learning';
-import RegistrationForm from './components/registrationForm';
 import TrendingSpeaker from './components/trendingSpeaker';
 function App() {
   return (
     <div >
-      <RegistrationForm />
       <AboutSession />
+      <AboutLiveSession/>
       <Learning />
       <Clients />
       <About />
---
+++ src/components/aboutLiveSession.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import first from "../assets/aboutLiveSession/first.svg";
+import second from "../assets/aboutLiveSession/second.svg";
+import third from "../assets/aboutLiveSession/third.svg";
+import forth from "../assets/aboutLiveSession/forth.svg";
+
+const AboutLiveSession = () => {
+  return (
+    <div className="text-center py-8 mb-4 md:mb-16">
+      <p className='text-3xl'>How <span className='text-blue-500'>Live Sessions</span> Work?</p>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-8 w-4/5 md:w-3/5 mx-auto">
+        <div className="flex flex-col items-center p-2 md:p-4 bg-white shadow-lg">
+          <img src={first} alt="Image 1" className="w-20 h-20 m-5" />
+          <p className="">Express the interest by Filling the form</p>
+        </div>
+
+        <div className="flex flex-col items-center p-2 md:p-4 bg-white shadow-lg">
+          <img src={second} alt="Image 2" className="w-20 h-20 m-5" />
+          <p className="">Finalize Speakers, Date and Time</p>
+        </div>
+
+        <div className="flex flex-col items-center p-2 md:p-4 bg-white shadow-lg">
+          <img src={third} alt="Image 3" className="w-20 h-20 m-5" />
+          <p className="">Make payment through sent Payment Link</p>
+        </div>
+
+        <div className="flex flex-col items-center p-2 md:p-4 bg-white shadow-lg">
+          <img src={forth} alt="Image 4" className="w-20 h-20 m-5" />
+          <p className="">Host your Fully Managed Live Session</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutLiveSession;
